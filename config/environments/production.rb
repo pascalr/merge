5c80a1f5795ae9abc2b67f96b9e39a7ccc427205
Dark Krael <dark.krael@gmail.com>
2020-12-16 15:47:20 -0500

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,12 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
+  
+  # Modified this as per here: https://stackoverflow.com/questions/12719541/css-loading-locally-but-not-in-heroku-for-a-rails-app
+  config.assets.compile = true
+  config.serve_static_assets = true
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
