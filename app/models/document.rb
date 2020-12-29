5adc689f6cac76a89637671fa63cecb64ac0470e
Pascal Rainville <pascal.rainville@gmail.com>
2020-12-29 09:47:44 -0500

    .

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,8 @@
 class Document < ApplicationRecord
   belongs_to :folder, optional: true
   has_rich_text :content
+  
+  def to_param
+    "#{id}-#{name}"
+  end
 end
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,4 +2,9 @@ class Folder < ApplicationRecord
   belongs_to :folder, optional: true
   has_many :folders
   has_many :documents
+  
+  def to_param
+    "#{id}-#{name}"
+  end
+
 end
