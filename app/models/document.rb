c5b315c58ee458a9ba9fcc7395a31c94e610a386
Pascal Rainville <pascal.rainville@gmail.com>
2021-01-09 17:03:51 -0500

    .

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,6 @@
 class DocumentValidator < ActiveModel::Validator
   def validate(record)
-    if record.raw_content and (record.content and not record.content.body.empty?)
+    if record.raw_content and (record.content and record.content.body and not record.content.body.empty?)
       record.errors.add :base, "Can't have a document with both content and raw content."
     end
   end
@@ -10,6 +10,10 @@ class Document < ApplicationRecord
   belongs_to :folder, optional: true
   has_rich_text :content
   validates_with DocumentValidator
+
+  def ancestors
+    folder ? ([folder] + folder.ancestors) : []
+  end
 
   def to_param
     "#{id}-#{name}"
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,6 +2,10 @@ class Folder < ApplicationRecord
   belongs_to :folder, optional: true
   has_many :folders
   has_many :documents
+
+  def ancestors
+    folder ? ([folder] + folder.ancestors) : []
+  end
   
   def to_param
     "#{id}-#{name}"
